4fdcc56f4d93f6450bf916975f40a675489cd98d
veeraadileep <dileepveeraa@gmail.com>
2026-01-31 12:24:24 +0530

    First commit

---
+++ src/Routes.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { BrowserRouter, Routes as RouterRoutes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
+import ErrorBoundary from "./components/ErrorBoundary";
+import NotFound from "./pages/NotFound";
+import DealsPage from './pages/deals';
+import Dashboard from './pages/dashboard';
+import Pipeline from './pages/pipeline';
+
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <ErrorBoundary>
+      <ScrollToTop />
+      <RouterRoutes>
+        {/* Define your route here */}
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/deals" element={<DealsPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/pipeline" element={<Pipeline />} />
+        <Route path="*" element={<NotFound />} />
+      </RouterRoutes>
+      </ErrorBoundary>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
